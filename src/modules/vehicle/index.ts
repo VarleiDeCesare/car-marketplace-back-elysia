f224bc48d8ecdbfa3dc7cea98f1b735bfde8a577
varlei_cesare <varlei.cesare@corebiz.ag>
2026-07-06 20:08:46 -0300

    feat: adding prefix to vehicles module

--- src/modules/vehicle/index.ts
+++ src/modules/vehicle/index.ts
@@ -4,16 +4,16 @@ import { createVehicleBody, vehicleResponse } from './model'
 import { VehicleService } from './service'
 import { appBetterAuth } from '../../lib/app-better-auth'
 
-export const vehicleModule = new Elysia({ name: 'vehicle.module' })
+export const vehicleModule = new Elysia({ name: 'vehicle.module', prefix: "/vehicles" })
     .use(appBetterAuth)
-    .get('/vehicles', () => {
+    .get('', () => {
         return VehicleService.list()
     }, {
         auth: true,
         response: t.Array(vehicleResponse),
     })
     .post(
-        '/vehicle',
+        '',
         async ({ body, set }) => {
             const vehicle = await VehicleService.create(body)
 
